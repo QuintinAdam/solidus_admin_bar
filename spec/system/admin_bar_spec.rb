4ec0091abd397452f80c87505f96afe693f98a8e
QuintinAdam <quintinjadam@gmail.com>
2021-07-20 08:05:10 -0600

    updates specs to pass rubocop

--- spec/system/admin_bar_spec.rb
+++ spec/system/admin_bar_spec.rb
@@ -2,148 +2,124 @@
 
 require 'spec_helper'
 
-context 'admin bar home page' do
-  context "an admin user" do
-    before do
-      sign_in_as_admin!
+describe 'Admin Bar', type: :system do
+  describe 'home page' do
+    context "when an admin user" do
+      before do
+        sign_in_as_admin!
+        visit spree.root_path
+      end
+
+      it "displays the admin bar and links to dashboard" do
+        within('#admin_bar'){ click_link 'Dashboard' }
+        expect(page).to have_current_path(spree.admin_orders_path, ignore_query: true)
+      end
+    end
+
+    it "does not allow a regular user to see the admin bar" do
+      sign_in_as_user!
       visit spree.root_path
+      expect(page).not_to have_content('Dashboard')
     end
 
-    it "displays the admin bar and links to dashboard" do
-      within('#admin_bar'){ click_link 'Dashboard' }
-      expect(page).to have_current_path(spree.admin_orders_path, ignore_query: true)
+    it "does not allow a guest user to see the admin bar" do
+      visit spree.root_path
+      expect(page).not_to have_content('Dashboard')
     end
   end
 
-  it "does not allow a regular user to see the admin bar" do
-    sign_in_as_user!
-    visit spree.root_path
-    expect(page).not_to have_content('Dashboard')
-  end
+  describe 'products admin bar' do
+    let!(:product) { create(:product, name: "Superman T-Shirt") }
 
-  it "does not allow a guest user to see the admin bar" do
-    visit spree.root_path
-    expect(page).not_to have_content('Dashboard')
-  end
-end
+    context "when an admin user" do
+      before do
+        sign_in_as_admin!
+        visit spree.root_path
+        click_link 'Superman T-Shirt'
+      end
 
-describe 'products admin bar' do
-  let!(:product) { create(:product, name: "Superman T-Shirt") }
+      it "can navigate to edit the product in the admin" do
+        within('#admin_bar'){ click_link 'Edit Product' }
+        expect(page).to have_current_path(spree.edit_admin_product_path(product), ignore_query: true)
+      end
 
-  context "an admin user" do
-    before do
-      sign_in_as_admin!
+      it "allows an admin user to edit product images in the admin" do
+        within('#admin_bar'){ click_link 'Images' }
+        expect(page).to have_current_path(spree.admin_product_images_path(product), ignore_query: true)
+      end
+
+      it "allows an admin user to edit variants in the admin" do
+        within('#admin_bar'){ click_link 'Variants' }
+        expect(page).to have_current_path(spree.admin_product_variants_path(product), ignore_query: true)
+      end
+
+      it "allows an admin user to edit prices in the admin" do
+        within('#admin_bar'){ click_link 'Prices' }
+        expect(page).to have_current_path(spree.admin_product_prices_path(product), ignore_query: true)
+      end
+
+      it "allows an admin user to edit properties in the admin" do
+        within('#admin_bar'){ click_link 'Properties' }
+        expect(page).to have_current_path(spree.admin_product_product_properties_path(product), ignore_query: true)
+      end
+
+      it "allows an admin user to edit stock in the admin" do
+        within('#admin_bar'){ click_link 'Stock' }
+        expect(page).to have_current_path(spree.admin_product_stock_path(product), ignore_query: true)
+      end
+    end
+
+    it "does not allow a regular user to navigate to the admin" do
+      sign_in_as_user!
       visit spree.root_path
       click_link 'Superman T-Shirt'
+      expect(page).not_to have_content('Edit Product')
     end
 
-    it "can navigate to edit the product in the admin" do
-      within('#admin_bar'){ click_link 'Edit Product' }
-      expect(page).to have_content(product.name)
-      expect(page).to have_current_path(spree.edit_admin_product_path(product), ignore_query: true)
+    it "does not allow a guest user to navigate to the admin" do
+      visit spree.root_path
+      click_link 'Superman T-Shirt'
+      expect(page).not_to have_content('Edit Product')
     end
-
-    it "allows an admin user to edit product images in the admin" do
-      within('#admin_bar'){ click_link 'Images' }
-      expect(page).to have_current_path(spree.admin_product_images_path(product), ignore_query: true)
-    end
-
-    it "allows an admin user to edit variants in the admin" do
-      within('#admin_bar'){ click_link 'Variants' }
-      expect(page).to have_current_path(spree.admin_product_variants_path(product), ignore_query: true)
-    end
-
-    it "allows an admin user to edit prices in the admin" do
-      within('#admin_bar'){ click_link 'Prices' }
-      expect(page).to have_current_path(spree.admin_product_prices_path(product), ignore_query: true)
-    end
-
-    it "allows an admin user to edit properties in the admin" do
-      within('#admin_bar'){ click_link 'Properties' }
-      expect(page).to have_current_path(spree.admin_product_product_properties_path(product), ignore_query: true)
-    end
-
-    it "allows an admin user to edit stock in the admin" do
-      within('#admin_bar'){ click_link 'Stock' }
-      expect(page).to have_current_path(spree.admin_product_stock_path(product), ignore_query: true)
-    end
-    # it "allows an admin user to edit related products in the admin" do
-    #   within('#admin_bar'){ click_link 'Related' }
-    #   expect(current_path).to eq(spree.related_admin_product_path(product))
-    # end
   end
 
-  it "does not allow a regular user to navigate to the admin" do
-    sign_in_as_user!
-    visit spree.root_path
-    click_link 'Superman T-Shirt'
-    expect(page).not_to have_content('Dashboard')
-    expect(page).not_to have_content('Edit Product')
-  end
+  describe 'taxons admin bar' do
+    let!(:taxonomy) { create(:taxonomy, name: "Category") }
+    let!(:taxon) { taxonomy.root.children.create(name: "Clothing", taxonomy_id: taxonomy.id) }
 
-  it "does not allow a guest user to navigate to the admin" do
-    visit spree.root_path
-    click_link 'Superman T-Shirt'
-    expect(page).not_to have_content('Dashboard')
-    expect(page).not_to have_content('Edit Product')
-  end
-end
+    context "when an admin user" do
+      before do
+        sign_in_as_admin!
+        visit spree.root_path
+        click_link 'Clothing'
+      end
 
-describe 'taxons admin bar' do
-  let!(:taxonomy) { create(:taxonomy, name: "Category") }
-  let!(:taxon) { taxonomy.root.children.create(name: "Clothing", taxonomy_id: taxonomy.id) }
+      it "can navigate to edit the taxonomy in the admin" do
+        within('#admin_bar'){ click_link 'Edit Taxonomy' }
+        expect(page).to have_current_path(spree.edit_admin_taxonomy_path(taxonomy), ignore_query: true)
+      end
 
-  context "an admin user" do
-    before do
-      sign_in_as_admin!
+      it "can navigate to edit the taxon in the admin" do
+        within('#admin_bar'){ click_link 'Edit Taxon' }
+        expect(page).to have_current_path(spree.edit_admin_taxonomy_taxon_path(taxonomy.id, taxon.id),
+          ignore_query: true)
+      end
+    end
+
+    it "does not allow a normal user to navigate to the admin" do
+      sign_in_as_user!
       visit spree.root_path
       click_link 'Clothing'
+      expect(page).not_to have_content('Edit Taxonomy')
     end
 
-    it "can navigate to edit the taxonomy in the admin" do
-      within('#admin_bar'){ click_link 'Edit Taxonomy' }
-      expect(page).to have_current_path(spree.edit_admin_taxonomy_path(taxonomy), ignore_query: true)
+    it "does not allow a guest user to navigate to the admin" do
+      visit spree.root_path
+      click_link 'Clothing'
+      expect(page).not_to have_content('Edit Taxonomy')
     end
-
-    it "can navigate to edit the taxon in the admin" do
-      within('#admin_bar'){ click_link 'Edit Taxon' }
-      expect(page).to have_current_path(spree.edit_admin_taxonomy_taxon_path(taxonomy.id, taxon.id), ignore_query: true)
-    end
-  end
-
-  it "does not allow a normal user to navigate to the admin" do
-    sign_in_as_user!
-    visit spree.root_path
-    click_link 'Clothing'
-    expect(page).not_to have_content('Dashboard')
-    expect(page).not_to have_content('Edit Taxonomy')
-    expect(page).not_to have_content('Edit Taxon')
-  end
-
-  it "does not allow a guest user to navigate to the admin" do
-    visit spree.root_path
-    click_link 'Clothing'
-    expect(page).not_to have_content('Dashboard')
-    expect(page).not_to have_content('Edit Taxonomy')
-    expect(page).not_to have_content('Edit Taxon')
   end
 end
-
-# describe 'pages admin bar' do
-#   let!(:store) { create(:store, default: true) }
-#   let!(:content_page) { Spree::Page.create!(slug: '/page2', title: 'TestPage2', body: 'Body2', visible: true, stores: [store]) }
-#   it "an admin user can navigate to edit the page in the admin" do
-#     sign_in_as_admin!
-#     visit '/page2'
-#     expect(page).to have_content('Edit Page')
-#     within('#admin_bar'){ click_link 'Edit Page' }
-#     current_path.expect == spree.edit_admin_page_path(content_page)
-#   end
-#   it "does not allow a regular user to navigate to the admin" do
-#     visit '/page2'
-#     page.expect_not have_content('Edit Page')
-#   end
-# end
 
 def sign_in_as_admin!
   user = create(:admin_user)
@@ -159,9 +135,4 @@ def sign_in_as_user!
   fill_in 'Email', with: user.email
   fill_in 'Password', with: 'secret'
   click_button 'Login'
-end
-
-def create_taxon
-  taxonomy = create(:taxonomy, name: "Category")
-  create(:taxon, name: "T-Shirts", parent: taxonomy.root, taxonomy: taxonomy)
 end
